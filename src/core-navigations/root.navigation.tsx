e574118aa7edb4022ef417e09c26bc026b7f976c
wadhwakushagra03@gmail.com <kishi1620>
2024-08-08 00:06:53 +0530

    bottom nav bar setup

--- src/core-navigations/root.navigation.tsx
+++ src/core-navigations/root.navigation.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react/react-in-jsx-scope */
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import DashboardStack from '../modules/dashboard/navigation/dashboard.navigation';
@@ -5,21 +6,72 @@ import {
   createNavigationContainerRef,
   NavigationContainer,
 } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {LocalSvg} from 'react-native-svg/css';
 
 export const Stack = createNativeStackNavigator();
 export const navigationRef = createNavigationContainerRef();
-const routes = [
-  {
-    name: 'dashboardStack',
-    component: DashboardStack,
-  },
-];
+// const routes = [
+//   {
+//     name: 'dashboardStack',
+//     component: DashboardStack,
+//   },
+// ];
+
+const Tab = createBottomTabNavigator();
+
+const MyTabs = () => {
+  return (
+    <Tab.Navigator initialRouteName="Product">
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <LocalSvg asset={require('../../assets/orders.svg')} />
+          ),
+        }}
+        name="Order"
+        component={DashboardStack}
+      />
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <LocalSvg asset={require('../../assets/inventory.svg')} />
+          ),
+        }}
+        name="Product"
+        component={DashboardStack}
+      />
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <LocalSvg asset={require('../../assets/currencyRupee.svg')} />
+          ),
+        }}
+        name="Overview"
+        component={DashboardStack}
+      />
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <LocalSvg asset={require('../../assets/person.svg')} />
+          ),
+        }}
+        name="Profile"
+        component={DashboardStack}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Root = () => {
   return (
     <NavigationContainer ref={navigationRef}>
       <Stack.Navigator initialRouteName={'dashboardStack'}>
-        {routes?.map(route => {
+        {/* {routes?.map(route => {
           return (
             <Stack.Screen
               key={route?.name}
@@ -28,7 +80,13 @@ const Root = () => {
               options={{headerShown: false}}
             />
           );
-        })}
+        })} */}
+        {/* <MyTabs /> */}
+        <Stack.Screen
+          name={'MyTabs'}
+          component={MyTabs}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
